a53bfc2668a2bedaccceca3d7fe88da60b307e73
yaphone <zhouyaphone@163.com>
2025-01-11 18:25:49 +0800

    dev
    dev

--- content-script.js
+++ content-script.js
@@ -3,49 +3,75 @@ let iframeDocument = null
 
 
 function getRemark(text) {
-	fetch("https://hewuhelper.hewuqi.tech/gpt/remark-helper?content=" + text, {
-		method: "GET", // 请求方法
-		headers: {
-			"Content-Type": "application/json" // 设置请求头
-		}
-	})
-	.then(response => response.text())
-	.then(data => {
-		const startIndex = data.lastIndexOf("data:")
-		const endIndex = data.lastIndexOf("}")
+	const remarkTextArea = iframeDocument.getElementById('recontent')
+	const wordsCount = iframeDocument.getElementById('Journal_TCount')
+	const passRadio = iframeDocument.querySelectorAll('[type="radio"]')[0]
+	const bRadio = iframeDocument.querySelectorAll('[type="radio"]')[3]
+	let eventSource = new EventSource('https://hewuhelper.hewuqi.tech/gpt/remark-helper?content=' + encodeURIComponent(text))
 
-		const content = data.substring(startIndex + 5, endIndex + 1)
-		const json = JSON.parse(content)
+	// 默认设置为通过  级别为B
+	passRadio.checked = true
+	bRadio.checked = true
+
+	eventSource.onmessage = function(event) {
+		const json = JSON.parse(event.data)
 		const text = json.output.text
-		const remarkTextArea = iframeDocument.getElementById('recontent')
 		remarkTextArea.value = text
+
+		wordsCount.value = text.length
+	}
+
+	// 监听特定的事件类型
+	eventSource.addEventListener('customEvent', function(event) {
+		console.log('Custom event:', event.data);
 	})
-	.catch(error => {
-		console.error("Error:", error); // 处理错误
-	});
+
+	// 处理连接打开的事件
+	eventSource.onopen = function(event) {
+		console.log('Connection established');
+	}
+
+	// 处理连接错误
+	eventSource.onerror = function(event) {
+		console.error('Error occurred:', event);
+		// 关闭连接，防止自动重连
+    eventSource.close();
+    console.log('Connection closed to prevent reconnection');
+	}
 }
 
-// 选择所有的链接
-const links = document.querySelectorAll('a');
+const currentUrl = window.location.href
 
-// 为每个链接绑定点击事件监听器
-links.forEach(link => {
-    link.addEventListener('click', function(event) {
-		setTimeout(() => {
-			const iframe = document.getElementById('layui-layer-iframe1')
-			if (iframe) {
-				iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-				const tdElement = iframeDocument.querySelectorAll('td[align="left"]')[1];
-				// 如果找到了该 td 元素
-				if (tdElement) {
-					// 获取该 td 元素中的文本内容
-						noteContent = tdElement.textContent.trim(); // 使用 textContent 获取文本并去除多余的空格
-						const text = "我是一名职业院校的教师，目前正在指导学生进行岗位实习，要求学生每周完成一篇周记，然后我需要针对周记内容进行批阅，将批阅意见反馈给学生，以下是一篇学生的周记内容，请针对周记内容，给学生一个反馈，50字左右。周记内容如下：" + noteContent
-						getRemark(text)
-				} else {
-					console.log('未找到目标 td 元素');
+// 选择所有的链接
+const links = document.querySelectorAll('a')
+	// 为每个链接绑定点击事件监听器
+	links.forEach(link => {
+			link.addEventListener('click', function(event) {
+			setTimeout(() => {
+				const iframe = document.getElementById('layui-layer-iframe1')
+				if (iframe) {
+					iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+					const tdElement = iframeDocument.querySelectorAll('td[align="left"]')[1];
+					// 如果找到了该 td 元素
+					if (tdElement) {
+						// 获取该 td 元素中的文本内容
+							noteContent = tdElement.textContent.trim(); // 使用 textContent 获取文本并去除多余的空格
+							chrome.storage.sync.get(['promptText'], function(result) {
+								if (!result.promptText) {
+									chrome.storage.sync.set({promptText: '我是一名职业院校的教师，目前正在指导学生进行岗位实习，要求学生每周完成一篇周记，然后我需要针对周记内容进行批阅，将批阅意见反馈给学生，以下是一篇学生的周记内容，请针对周记内容，给学生一个反馈，50字左右，不要出现套话。'}, function() {
+										window.close();
+									});
+								} else {
+									const promptText = result.promptText
+									const text = promptText + "。学生周记内容如下：" + noteContent
+									getRemark(text)
+								}
+							});
+					} else {
+						console.log('未找到目标 td 元素');
+					}
 				}
-			}
-		}, 1000)
-    });
-});
+			}, 1000)
+			});
+	});
+
---
+++ js/popup.js
@@ -0,0 +1,30 @@
+﻿// 检查是否有提示文本，如果没有，则设置默认
+chrome.storage.sync.get(['promptText'], function(result) {
+  if (!result.promptText) {
+    chrome.storage.sync.set({promptText: '我是一名职业院校的教师，目前正在指导学生进行岗位实习，要求学生每周完成一篇周记，然后我需要针对周记内容进行批阅，将批阅意见反馈给学生，以下是一篇学生的周记内容，请针对周记内容，给学生一个反馈，50字左右，不要出现套话。'}, function() {
+      window.close();
+    });
+  }
+});
+
+function confirmPromptText(){
+  var promptText = document.getElementById("promptText").value;
+  chrome.storage.sync.set({promptText: promptText}, function() {
+    alert('保存成功！')
+    setTimeout(() => {
+      window.close();
+    }, 100)
+  });
+}
+
+function loadPromptText(){
+  chrome.storage.sync.get(['promptText'], function(result) {
+    document.getElementById("promptText").value = result.promptText;
+  });
+}
+
+document.getElementById('confirmButton').addEventListener('click', function() {
+  confirmPromptText();
+});
+
+document.getElementById('promptText').value = loadPromptText()
