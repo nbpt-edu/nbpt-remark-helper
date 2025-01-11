87f8a08b0f1bb9702f3f608b0355f98f8b865e2e
yaphone <zhouyaphone@163.com>
2025-01-11 11:20:30 +0800

    dev
    dev

--- content-script.js
+++ content-script.js
@@ -1,3 +1,31 @@
-﻿(function() {
-	console.log('这是 simple-chrome-plugin-demo 的content-script！');
-})();
+﻿const observer = new MutationObserver((mutationsList, observer) => {
+	for (let mutation of mutationsList) {
+		if (mutation.type === 'childList') {
+			setTimeout(() => {
+				const iframe = document.getElementById('layui-layer-iframe1')
+				if (iframe) {
+					const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+					const tdElement = iframeDocument.querySelectorAll('td[align="left"]')[1];
+					// 如果找到了该 td 元素
+					if (tdElement) {
+						// 获取该 td 元素中的文本内容
+						const textContent = tdElement.textContent.trim(); // 使用 textContent 获取文本并去除多余的空格
+						console.log('周记内容：', textContent);
+					} else {
+						console.log('未找到目标 td 元素');
+					}
+				}
+			}, 2000)
+		}
+	  }
+})
+
+function init() {
+}
+
+init()
+
+// 配置观察选项
+const config = { childList: true, subtree: true };
+// 开始观察页面的 body 元素
+observer.observe(document.body, config);
